f3857dc9b1cf57421d3afd80400ceaa3d254f0bd
Willow Chau <willowachau@gmail.com>
2018-07-11 20:44:03 -0400

    about page

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -3,7 +3,7 @@ const team =
     {name:"Arianna Perry", email:"perry.arianna83@gmail.com"},
     {name:"Angie Martinez", email:"angie.martinez@students.excelacaedemy.org"},
     {name:"Amaya Mayhew", email:""},
-    {name:"Chami Lamelas", email:""},
+    {name:"Chami Lamelas", email:"chami.lamelas@gmail.com"},
     {name:"Willow Chau", email:"willowachau@gmail.com"}
   ];
 
