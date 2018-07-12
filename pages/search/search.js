f6fff14d92b7e6581122f8ef66af988c40441343
Amayarose <amayarosemayehw@gmail.com>
2018-07-12 10:33:15 -0400

    search

---
+++ pages/search/search.js
@@ -0,0 +1,8 @@
+const Trending =
+  [
+    {topic :"Kpop music culture"},
+    {topic:"Fifa World Cup Results"},
+    {topic:"How's Twenty one Pilots new music, Trenches?" },
+    {topic:"Trumps comments on NATO" }
+  ];
+  
