e11e4ba933c5ee370fd3724ad6ed1ec651fa1854
Willow Chau <willowachau@gmail.com>
2018-07-13 09:57:25 -0400

    about page bio

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -5,7 +5,7 @@ const team =
     {name:"Amaya Mayhew",email:"amayarosemayhew@gmail.com",bio:"I am a programmer from Yonkers, New York. I have 1 year experience in programming experience in html, css and javascript. "},
 
     {name:"Chami Lamelas", email:"chami.lamelas@gmail.com",bio:""},
-    {name:"Willow Chau", email:"willowachau@gmail.com",bio:""}
+    {name:"Willow Chau", email:"willowachau@gmail.com",bio:"I am a rising high school senior from Groton, Massachusetts. I have experience in javascript, python, and c++."}
   ];
 
 Template.about.helpers(
