5d915244b91bf600bd383942b0d61c361894dc2e
angie <angie.martinez@students.excelacademy.org>
2018-07-13 10:36:51 -0400

    added bio

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -1,7 +1,7 @@
 const team =
   [
     {name:"Arianna Perry", email:"perry.arianna83@gmail.com",bio:""},
-    {name:"Angie Martinez",email:"angie.martinez@students.excelacaedemy.org",bio:""},
+    {name:"Angie Martinez",email:"angie.martinez@students.excelacademy.org",bio:"I am a rising junior and 15 year old programmer. I have expirience with coding language such as html,javascript, and python. I have been coding for about a year now and hope to further continue my career in coding."},
     {name:"Amaya Mayhew",email:"amayarosemayhew@gmail.com",bio:"I am a programmer from Yonkers, New York. I have 1 year experience in programming experience in html, css and javascript. "},
 
     {name:"Chami Lamelas", email:"chami.lamelas@gmail.com",bio:""},
