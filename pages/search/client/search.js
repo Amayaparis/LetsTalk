d1ab881a4693e201df959725c5b195dd35f23d03
Amayarose <amayarosemayehw@gmail.com>
2018-07-12 20:25:23 -0400

    delete extra templates

--- pages/search/client/search.js
+++ pages/search/client/search.js
@@ -3,10 +3,10 @@ Template.search.helpers(
       trending(){return trending}
     }
   )
-  
+
 const trending =
   [
-    {topic :"Kpop music culture"},
+    {topic:"Kpop music culture"},
     {topic:"Fifa World Cup Results"},
     {topic:"How's Twenty one Pilots new music?" },
     {topic:"Trumps comments on NATO" }
