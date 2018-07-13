b4a4aff6a751f3eaa5d467f7849fe83a54075c8f
Amayarose <amayarosemayehw@gmail.com>
2018-07-13 11:51:27 -0400

    delelte

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -2,12 +2,11 @@ const team =
   [
     {name:"Arianna Perry", email:"perry.arianna83@gmail.com",bio: "Im a rising Junior with a year of computer science experience. This fall I will go into AP computer science and senior year i will be doing cyber security."},
     {name:"Angie Martinez",email:"angie.martinez@students.excelacademy.org",bio:"I am a rising junior and 15 year old programmer. I have expirience with coding language such as html,javascript, and python. I have been coding for about a year now and hope to further continue my career in coding."},
-<<<<<<< HEAD
-    {name:"Amaya Mayhew",email:"amayarosemayhew@gmail.com",bio:"I am a programmer from Yonkers, New York. I have 1 year experience in programming experience in html, css and javascript. "},
-=======
-    {name:"Amaya Mayhew",email:"amayarosemayhew@gmail.com",bio:"I am a rising senior from Yonkers, New York. I have experience in programming in languages such as html, css, javascript, and little in Ardiuno . I'm intersted in pursing a career that involes programming infused with another STEM field."},
 
->>>>>>> 279cb1615d5aa04e252aa1202951c498594f8f47
+    {name:"Amaya Mayhew",email:"amayarosemayhew@gmail.com",bio:"I am a programmer from Yonkers, New York. I have 1 year experience in programming experience in html, css and javascript. "},
+
+    {name:"Amaya Mayhew",email:"amayarosemayhew@gmail.com",bio:"I am a rising senior from Yonkers, New York. I have experience in programming in languages such as html, css, javascript,and little in Ardiuno . I'm intersted in pursing a career that involes programming infused with another STEM field."},
+
     {name:"Chami Lamelas", email:"chami.lamelas@gmail.com",bio:""},
     {name:"Willow Chau", email:"willowachau@gmail.com",bio:"I am a rising high school senior from Groton, Massachusetts. I have experience in javascript, python, and c++."},
   ];
