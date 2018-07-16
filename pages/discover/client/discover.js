8f3f9da18fcb576d5f200fa2665ebc84f2fa2d07
Amayarose <amayarosemayehw@gmail.com>
2018-07-16 16:00:28 -0400

    discover events

--- pages/discover/client/discover.js
+++ pages/discover/client/discover.js
@@ -16,7 +16,10 @@ const ourSponsors = [
 
 const ourEvents = [
   {name:" Women's March NYC 2019 ",date:"Sat, April 20, 2019",
-  photo:"https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F42557726%2F249281032365%2F1%2Foriginal.jpg?w=800&auto=compress&rect=294%2C0%2C2354%2C1177&s=71c37e544ac55dd1adb08eefcdb64ff8", link:"https://www.eventbrite.com/e/womens-march-nyc-2019-tickets-44430426602?aff=ebdssbdestsearch"}
+  photo:"https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F42557726%2F249281032365%2F1%2Foriginal.jpg?w=800&auto=compress&rect=294%2C0%2C2354%2C1177&s=71c37e544ac55dd1adb08eefcdb64ff8", link:"https://www.eventbrite.com/e/womens-march-nyc-2019-tickets-44430426602?aff=ebdssbdestsearch"},
+  {name:"Zero Hour NYC Youth Climate March",date:"Sat, July 21, 2018",
+  photo:"https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F44688665%2F176477533446%2F1%2Foriginal.jpg?w=800&auto=compress&rect=0%2C192%2C720%2C360&s=582f26c1c613e9ccb0a058e63b1ac1fb", link:"https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F44688665%2F176477533446%2F1%2Foriginal.jpg?w=800&auto=compress&rect=0%2C192%2C720%2C360&s=582f26c1c613e9ccb0a058e63b1ac1fb"},
+  {name:"2018 Girls Matter Walk",date:"Sun, September 23, 2018",photo:"https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F45933676%2F7318641369%2F1%2Foriginal.jpg?w=800&auto=compress&rect=0%2C390%2C720%2C360&s=35b1184cdf7f9f1a01530bc1869fb3d4",link:"https://www.eventbrite.com/e/2018-girls-matter-walk-tickets-46918658972?aff=ebdssbdestsearch"}
 ]
 Template.discover.helpers({
 	eventsData: function() {return ourEvents},
