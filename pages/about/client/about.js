62c26e9eb87fce7d831f7950a9a0c8f66b85b24c
Amayarose <amayarosemayehw@gmail.com>
2018-07-16 16:01:43 -0400

    update about page

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -1,13 +1,10 @@
 const team =
   [
     {name:"Arianna Perry", email:"perry.arianna83@gmail.com",bio: "I'm a rising Junior with a year of computer science experience. This fall I will go into AP computer science and senior year I will be doing cyber security."},
-<<<<<<< HEAD
     {name:"Angie Martinez",email:"angie.martinez@students.excelacademy.org",bio:"I am a rising junior and a 15 year old programmer. I have expirience with coding languages such as html, javascript, and python. I have been coding for about a year now and move on to AP computer sceince this fall at my highschool to further my programming career."},
     {name:"Amaya Mayhew",email:"amayarosemayhew@gmail.com",bio:"I am a rising senior from Yonkers, New York. I have experiences programming in languages such as html, css, javascript, and Arduino . I'm interested in pursing a career that involes programming infused with another STEM field."},
-=======
     {name:"Angie Martinez",email:"angie.martinez@students.excelacademy.org",bio:"I am a rising junior and a 15 year old programmer. I have expirience with coding languages such as html, javascript, and python. I have been coding for about a year now and move on to AP computer science this fall at my highschool to further my programming career."},
     {name:"Amaya Mayhew",email:"amayarosemayhew@gmail.com",bio:"I am a rising senior from Yonkers, New York. I have experience in programming in languages such as html, css, javascript, and a little in Arduino. I'm intersted in pursing a career that involves programming infused with another STEM field."},
->>>>>>> 3de3fb8b11f55804504e4334ff813a1065321db5
     {name:"Chami Lamelas", email:"chami.lamelas@gmail.com",bio:"I am going to be a freshman at Brandeis in the fall, I live in Worcester, MA. I have used Java for 2.5 years but have used half a dozen or so other languages."},
     {name:"Willow Chau", email:"willowachau@gmail.com",bio:"I am a rising high school senior from Groton, Massachusetts. I have experience in javascript, python, and c++."},
   ];
