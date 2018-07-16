ce66bc125b76004d49098901b1458c37ab9f7c28
Chami L <chami.lamelas@gmail.com>
2018-07-16 11:15:36 -0400

    Fixed Profile Bug, Updated Polls
    Fixed Profile Bug
    - Added check() function to route to desired page after profile creation
    if no profile exists for the user. Before, pages would throw errors that
    would require profiles to exist.
    Updated Polls
    - Polls now close when the end time has been reached and polls now
    display the number of yes and no votes if the user has voted already or
    if the poll.

---
+++ client/layout/layout.js
@@ -0,0 +1,23 @@
+const PREV_ID = "prevPage";
+Session.set(PREV_ID,"");
+
+Template.layout.helpers({
+  check() {
+    if (Meteor.userId()) {
+      if (!Profiles.findOne({owner:Meteor.userId()})) {
+        Router.go("profilepage");
+        if (Router.current().route.getName() != "profilepage")
+          Session.set(PREV_ID,Router.current().route.getName());
+        console.log("current route="+Router.current().route.getName());
+        console.log("User is logged in with no profile.");
+      }
+      else {
+        Router.go(Session.get(PREV_ID));
+        console.log("User is logged in with a profile.");
+      }
+    }
+    else {
+      console.log("User is not logged in.")
+    }
+  }
+})
