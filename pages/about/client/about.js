fadd4be4425d865574de3c351418a4ae6cb9d2d4
angie <angie.martinez@students.excelacademy.org>
2018-07-16 15:23:13 -0400

    typos

--- pages/about/client/about.js
+++ pages/about/client/about.js
@@ -1,8 +1,8 @@
 const team =
   [
     {name:"Arianna Perry", email:"perry.arianna83@gmail.com",bio: "I'm a rising Junior with a year of computer science experience. This fall I will go into AP computer science and senior year I will be doing cyber security."},
-    {name:"Angie Martinez",email:"angie.martinez@students.excelacademy.org",bio:"I am a rising junior and a 15 year old programmer. I have expirience with coding languages such as html, javascript, and python. I have been coding for about a year now and move on to AP computer sceince this fall at my highschool to further my programming career."},
-    {name:"Amaya Mayhew",email:"amayarosemayhew@gmail.com",bio:"I am a rising senior from Yonkers, New York. I have experience in programming in languages such as html, css, javascript, and a little in Ardiuno . I'm intersted in pursing a career that involes programming infused with another STEM field."},
+    {name:"Angie Martinez",email:"angie.martinez@students.excelacademy.org",bio:"I am a rising junior and a 15 year old programmer. I have expirience with coding languages such as html, javascript, and python. I have been coding for about a year now and move on to AP computer science this fall at my highschool to further my programming career."},
+    {name:"Amaya Mayhew",email:"amayarosemayhew@gmail.com",bio:"I am a rising senior from Yonkers, New York. I have experience in programming in languages such as html, css, javascript, and a little in Ardiuno. I'm intersted in pursing a career that involves programming infused with another STEM field."},
     {name:"Chami Lamelas", email:"chami.lamelas@gmail.com",bio:"I am going to be a freshman at Brandeis in the fall, I live in Worcester, MA. I have used Java for 2.5 years but have used half a dozen or so other languages."},
     {name:"Willow Chau", email:"willowachau@gmail.com",bio:"I am a rising high school senior from Groton, Massachusetts. I have experience in javascript, python, and c++."},
   ];
